1b2d4ee7a02b051712233861c4bdb4b6b0717a86
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-29 12:36:55 +0530

    New file forloop.py is added

---
+++ forloop.py
@@ -0,0 +1,15 @@
+for i in range(1,10,2):
+    print(i)
+
+str = "Welcome"
+for ch in str:
+    print(ch)
+
+lst=[1,23,25,25,36,25]
+for items in lst:
+    print(items)
+
+
+for i in range(10,0,-1):
+    print(i)
+
