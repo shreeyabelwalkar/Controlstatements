38c9665089ecbb828b28614141c85e3de9259059
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-29 12:05:10 +0530

    New file ifelif.py is added

---
+++ ifelif.py
@@ -0,0 +1,13 @@
+num=int(input("Enter any number between 1 to 5:"))
+if num==1:
+    print("one")
+elif num==2:
+    print("two")
+elif num==3:
+    print("three")
+elif num==4:
+    print("four")
+elif num==5:
+    print("five")
+else:
+    print("Number is not between 1 to 5")
