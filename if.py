aa3bfe5bca37eb2f4c15379852069b2000bdf5fc
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-29 11:46:39 +0530

    New file if.py is added

---
+++ if.py
@@ -0,0 +1,23 @@
+num=1
+if num==1:
+    print("one")
+print("End of the program")
+
+
+a=10
+b=20
+if a>b:
+    print("a is greater than b")
+print("b is greater than a")
+
+x=10
+y=30
+if x==10:
+    print("a")
+    print("b")
+    print("c")
+    if y==20:
+        print("d")
+        print("e")
+print("End of the programme")
+
